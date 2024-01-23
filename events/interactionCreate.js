c394f58109b460bda550fa38b1d25ea65b68a379
João Chung <52529400+Rodaviva29@users.noreply.github.com>
2024-01-23 17:09:36 +0000

    Support for Cooldown for slashCommands added.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events, InteractionType } = require('discord.js');
+const { Events, InteractionType, Collection  } = require('discord.js');
  
 module.exports = {
     name: Events.InteractionCreate,
@@ -8,6 +8,29 @@ module.exports = {
         if (interaction.isCommand()) {
             const command = interaction.client.commands.get(interaction.commandName);
             const database = await interaction.client.database
+
+            const { cooldowns } = interaction.client;
+
+            if (!cooldowns.has(command.data.name)) {
+                cooldowns.set(command.data.name, new Collection());
+            }
+            
+            const now = Date.now();
+            const timestamps = cooldowns.get(command.data.name);
+            const defaultCooldownDuration = 3;
+            const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+            
+            if (timestamps.has(interaction.user.id)) {
+                const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+            
+                if (now < expirationTime) {
+                    const expiredTimestamp = Math.round(expirationTime / 1_000);
+                    return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+                }
+            }
+    
+            timestamps.set(interaction.user.id, now);
+            setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
             
             if (!command) {
                 console.error(`No command matching ${interaction.commandName} was found.`);
