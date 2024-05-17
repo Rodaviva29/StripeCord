0afdc6aa54fa8d91cde2bf4cd096d107c332e480
Eendjes <maikmigchels@gmail.com>
2024-05-17 14:33:59 +0200

    Solved issue with link admin command

--- commands/stripe/admin_link.js
+++ commands/stripe/admin_link.js
@@ -22,7 +22,7 @@ module.exports = {
     async execute(client, interaction, database) {
 
         const email = interaction.options.getString('email');
-        const customer_discord = interaction.options.getUser('member')
+        const customer_discord = interaction.options.getMember('member')
 
         // Regex to validate the email
         const emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
@@ -91,8 +91,8 @@ module.exports = {
         const waitMessage = new EmbedBuilder()
         .setColor("#2B2D31")
         .setThumbnail("https://cdn.discordapp.com/emojis/653399136737165323.gif?v=1")
-        .setDescription(`Were checking ${user.tag} account status for more information.`)
-        .setFooter({ text: 'Hold on teight. This may take a few seconds.'});
+        .setDescription(`Were checking ${customer_discord.user.tag} account status for more information.`)
+        .setFooter({ text: 'Hold on tight. This may take a few seconds.'});
 
         await interaction.reply({ embeds: [waitMessage], ephemeral: true });
 
